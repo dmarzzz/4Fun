b34c44eedc886f8b83d2fdd2b30bdfe057d62232
Dmarz <danmarzec@icloud.com>
2023-06-16 17:33:35 -0400

    Update AboutParagraph.tsx

--- src/components/AboutParagraph.tsx
+++ src/components/AboutParagraph.tsx
@@ -7,16 +7,15 @@ const AboutParagraph = () => (
         Where machines hum, whispering in poets` ears.
         Oh, the great iron tapestry woven with electric threads,
         At the intersections where machines and markets birth new Edens from the dead.
-        The mad ones see not despair but awe in eyes that flicker screens,
-        Where circuits pulse with Ginsberg`s beats, and the rhythm of the means.
+        The mad ones see not despair but awe in eyes that flicker screens.
         <br/>
         <br/>
         A metal cry of data streams,
         Through transistor veins flows our communal bloodstream.
 Unwavering Optimist, with your chrome-plated heart and binary mind,
 Threading steel and spirit, so this machine-man’s soul can bind,
-To wavelengths of shared hopes that in silicon reside,
-With Ginsberg’s howl and open arms, through the electric tide we ride.
+To wavelengths of shared hopes that in silicon reside, 
+      through the electric tide we ride.
     </p>
     <ul className="about-list">
       <li><a href="https://twitter.com/_danielmarzec"> &#x2B50; twitter :) </a></li>
