7090298afe0887e65bb8a60dafcfd27aa2ec5500
dmarzzz <danmarzec@icloud.com>
2024-02-04 17:10:15 -0500

    new about me

--- src/components/AboutParagraph.tsx
+++ src/components/AboutParagraph.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const AboutParagraph = () => (
   <div className="about-paragraph-container">
     <p className="about-paragraph">
-    In fields where wire blossoms grow, behold the dawn of gears,
+        In fields where wire blossoms grow, behold the dawn of gears,
         Where machines hum, whispering in poets` ears.
-        Oh, the great iron tapestry woven with electric threads,
+        Oh, the great cybernetic tapestry woven with electric threads,
         At the intersections where machines and markets birth new Edens from the dead.
         The mad ones see not despair but awe in eyes that flicker screens.
         <br/>
@@ -14,8 +14,8 @@ const AboutParagraph = () => (
         Through transistor veins flows our communal bloodstream.
 Unwavering Optimist, with your chrome-plated heart and binary mind,
 Threading steel and spirit, so this machine-man’s soul can bind,
-To wavelengths of shared hopes that in silicon reside, 
-      through the electric tide we ride.
+As we navigate the digital surge, our collective dreams take flight,
+uniting flesh and circuit in the dawn's emerging light
     </p>
     <ul className="about-list">
       <li><a href="https://twitter.com/_danielmarzec"> &#x2B50; twitter :) </a></li>
