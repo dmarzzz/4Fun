d15326d9ed99f3936b2f569410007aa5333ecacf
dmarz <danmarzec@icloud.com>
2024-05-12 15:22:34 -0400

    Update FolderStructure.tsx

--- src/components/FolderStructure.tsx
+++ src/components/FolderStructure.tsx
@@ -8,7 +8,7 @@ const FolderStructure: React.FC<FolderStructureProps> = ({ flickerRate }) => {
   return (
     <ul className="folder-structure mt-4 neon-glow" style={{ animation: `flicker ${flickerRate}s infinite` }}>
       <li>
-        daniel_marzec/
+        daniel_marz/
         <ul>
           <li className="relative before:border-l ml-4 before:mr-4 before:border-b before:absolute before:-top-[3px] before:-left-3 before:w-3 before:h-4 border-white">
             about/
